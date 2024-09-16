274d001c4d399b5e2a70a07ee74d8c6441d21f08
MVagor <mvagor@yandex.ru>
2024-09-16 16:58:04 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second == 0:
+        a = 'Ошибка'
+        return a
+    else:
+        a = first / second
+        return a
---
+++ true_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second == 0:
+        a = from math import inf
+        return a
+    else:
+        a = first / second
+        return a
